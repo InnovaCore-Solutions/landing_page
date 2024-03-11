fc02b234d95c34c748d7e5d85199aed6fc0a00a3
FranM <franmartinaguilar@gmail.com>
2024-03-11 12:06:45 +0100

    no message

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
-  basePath: "/"
+  basePath: '',
   // i18n: {
   //   locales: ["en","es"],
   //   defaultLocale: "es",
